58c050ca949a1fdd63bd218b03633228cc30ac01
Anirudh Rao <anirudh.rao@globalquakemodel.org>
2015-09-08 14:00:13 +0200

    Log LaTeX compilation errors and warnings in build script

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
-pdflatex -interaction=nonstopmode rmtk-docs.tex
-makeindex rmtk-docs.idx -s configuration/StyleInd.ist
-bibtex rmtk-docs
-makeglossaries rmtk-docs
-pdflatex -interaction=nonstopmode rmtk-docs.tex
-pdflatex -interaction=nonstopmode rmtk-docs.tex
+pdflatex -interaction=nonstopmode rmtk-docs.tex &> log.md
+bibtex rmtk-docs > log.md
+pdflatex -interaction=nonstopmode rmtk-docs.tex &> log.md
+pdflatex -interaction=nonstopmode rmtk-docs.tex &> log.md
+makeindex rmtk-docs.idx -s configuration/StyleInd.ist &> log.md
+makeglossaries rmtk-docs &> log.md
+pdflatex -interaction=nonstopmode rmtk-docs.tex &> log.md
+cat log.md | egrep "Error|Warning"
 ./clean.sh
 open rmtk-docs.pdf
